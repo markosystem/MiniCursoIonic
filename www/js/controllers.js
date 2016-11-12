c620c9a001515e52b3f556f668a026671211ad00
Marcos Batista <markos.system@gmail.com>
2016-11-12 00:16:59 -0300

    Incluído plugin Lockr para manipulação de LocalStorage

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,29 +5,29 @@ angular.module('starter.controllers',['ionic','ionic-toast'])
 	initialize();
 	getLocalStorage();
 	$scope.adicionar = function(){
-		if($scope.nova.nome){
-			var existe = false;
-			if($scope.tarefas.length > 0){
-				for(var i=0;i<$scope.tarefas.length;i++){
-					if($scope.nova.nome == $scope.tarefas[i].nome){
-						existe = true;
-					}
+		if(!$scope.nova.nome){
+			showMessage('Informe um nome!','top',false);
+			return;
+		}
+		var existe = false;
+		if($scope.tarefas.length > 0){
+			for(var i=0;i<$scope.tarefas.length;i++){
+				if($scope.nova.nome == $scope.tarefas[i].nome){
+					existe = true;
 				}
 			}
-			if(!existe){
-				$scope.tarefas.push({
-					nome: $scope.nova.nome,
-					feito: false
-				});
-				localStorage.setItem("tarefas", JSON.stringify($scope.tarefas));
-				totais();
-				$scope.nova = {nome: ''};
-				showMessage('Produto cadastrado com sucesso!','top',false);
-			}else{
-				showMessage('O Produto já existe na lista!','top',false);
-			}
+		}
+		if(!existe){
+			$scope.tarefas.push({
+				nome: $scope.nova.nome,
+				feito: false
+			});
+			Lockr.set('tarefas', $scope.tarefas);
+			totais();
+			$scope.nova = {nome: ''};
+			showMessage('Produto cadastrado com sucesso!','top',false);
 		}else{
-			showMessage('Informe um nome!','top',false);
+			showMessage('O Produto já existe na lista!','top',false);
 		}
 	};
 
@@ -39,8 +39,8 @@ angular.module('starter.controllers',['ionic','ionic-toast'])
 			}
 		}
 		if($scope.tarefas.length != naoMarcados.length){
-			localStorage.setItem("tarefas", JSON.stringify(naoMarcados));
-			$scope.tarefas = JSON.parse(localStorage.getItem('tarefas'));
+			Lockr.set('tarefas', naoMarcados);
+			$scope.tarefas = Lockr.get('tarefas');
 			showMessage('Produtos marcados excluídos com sucesso!','bottom',false);
 			totais();			
 		}else{
@@ -57,8 +57,8 @@ angular.module('starter.controllers',['ionic','ionic-toast'])
 			}
 		}
 		if($scope.tarefas.length != marcados.length){
-			localStorage.setItem("tarefas", JSON.stringify(marcados));
-			$scope.tarefas = JSON.parse(localStorage.getItem('tarefas'));
+			Lockr.set('tarefas', marcados);
+			$scope.tarefas = Lockr.get('tarefas');
 			showMessage('Produtos Não marcados excluídos com sucesso!','bottom',false);	
 			totais();		
 		}else{
@@ -91,8 +91,8 @@ angular.module('starter.controllers',['ionic','ionic-toast'])
 
 		$scope.marcados = {total: totalMarcados};
 		$scope.naoMarcados = {total: totalNaoMarcados};
-		localStorage.setItem("tarefas", JSON.stringify(tarefas));
-		$scope.tarefas = JSON.parse(localStorage.getItem('tarefas'));
+		Lockr.set('tarefas', tarefas);
+		$scope.tarefas = Lockr.get('tarefas');
 	}
 
 	function initialize(){
@@ -102,20 +102,20 @@ angular.module('starter.controllers',['ionic','ionic-toast'])
 	}
 
 	function getLocalStorage(){
-		if(localStorage.getItem('tarefas') == null){
+		if(Lockr.get('tarefas') == null){
 			$scope.tarefas = [];
 		}else{
-			$scope.tarefas = JSON.parse(localStorage.getItem('tarefas'));
+			$scope.tarefas = Lockr.get('tarefas');
 		}
 		totais();
 		return $scope.tarefas;
 	}
 
 	function showMessage(message, position, stick){
-	  	ionicToast.show(message, position, stick, 3000);		
+		ionicToast.show(message, position, stick, 3000);		
 	}
 	
 	$scope.hideToast = function(){
-	  ionicToast.hide();
+		ionicToast.hide();
 	};
 }])
